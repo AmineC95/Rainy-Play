8d86901b200001c7190a717dbe448f5ee857e17c
AmineC95 <87375880+AmineC95@users.noreply.github.com>
2022-03-22 18:34:21 +0100

    Add files via upload

---
+++ r_play/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>R Play</title>
+    <a><link rel="icon" href="/img/RainyPlay_Logo.jpg"></a>
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <div><img src="img/logo.png" alt="logo"></div>
+    <footer>    
+        <a href="connexion.php" ><button class="button">Connexion</button></a>
+        <a href="inscription.html"><button class="button">Inscription</button></a>
+        <a href="identifiants.php"> <button class="button">Admin</button></a>
+    </footer>
+    <main></main>
+</body>
+
+</html>
